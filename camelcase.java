e18e90999b43514ef6d642d4a9eb071c8bf33b60
Mahamood <shaikmisba305@gmail.com>
2024-12-10 14:19:37 +0530

    Add files via upload

---
+++ camelcase.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a=sc.nextLine();
+        String res="";
+        for(int i=0;i<a.length();i++)
+        {
+            char s=a.charAt(i);
+            s^=32;
+            res+=s;
+        }
+       System.out.println(res);
+    }
+}
