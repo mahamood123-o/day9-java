8185712a6250766f44cc04bd6928446bc0b80d85
Mahamood <shaikmisba305@gmail.com>
2024-12-10 11:00:18 +0530

    Add files via upload

---
+++ item.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class item
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a[]={"Apple","Banana","Cherry"};
+        for(String b:a)
+        System.out.println(b+",");
+    }
+}
