daf127a202fdc1ab83aac1871ce9f8b6eef5731a
Mahamood <shaikmisba305@gmail.com>
2024-12-10 14:48:17 +0530

    Add files via upload

--- countchar.java
+++ countchar.java
@@ -8,5 +8,12 @@ class Main {
             c[((int)(a.charAt(i)))-65]++;
         }
        System.out.println(Arrays.toString(c));
+       String a1="dharshan";
+       int c1[]=new int[26];
+       for(int i=0;i<a.length();i++)
+       {
+        c1[((int)(a1.charAt(i)))-97]++;
+       }
+       System.out.println(Arrays.toString(c1));
     }
 }
