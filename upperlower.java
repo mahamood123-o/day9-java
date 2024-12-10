4dbc5821668da998e52a03d94f52f0257b97be74
Mahamood <shaikmisba305@gmail.com>
2024-12-10 13:57:14 +0530

    Add files via upload

---
+++ upperlower.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char s=sc.next().charAt(0);
+        if((s&32)==0) System.out.println("Upper Case");
+        else System.out.println("Lower Case");
+    }
+}
